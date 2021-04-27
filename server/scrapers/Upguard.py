dc5422d19dbc983148bc1d989daf40841c0bf29b
NivAm12 <nivamos12@gmail.com>
2021-04-27 14:59:03 +0300

    Update name of abstract class

--- server/scrapers/Upguard.py
+++ server/scrapers/Upguard.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from WebScraper import CyberWebSearcher
+from  CyberWebSearcher import CyberWebSearcher
 import json
 
 
