105f8b767d81a477cb9577381e7c12ba9d499d94
royaboudi1 <royaboudi1@gmail.com>
2021-04-28 22:38:02 +0300

    landingpage improvement

--- client/negative-news/src/components/LandingPage.js
+++ client/negative-news/src/components/LandingPage.js
@@ -11,31 +11,77 @@ import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
 import ArticleCard from "./ArticleCard";
 import { CircularProgress } from "@material-ui/core";
+import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles({
   container: {
     boxShadow: "0px 0px 16px rgba(0, 0, 0, 0.2)",
     minHeight:"1000px",
     background: "#f7f7f7",
+    padding: "0 0 0 0"
+  }, 
+  header: {
+    background:'rgba(0,0,0,0.03)', 
+    paddingBottom: "30px", 
+    borderBottom: "3px solid rgba(0,0,0,1)",
+    paddingRight: "20px",
+    paddingLeft: "20px"
+  },
+  title:{
+    fontSize: "5rem",
+    color: "#3f51b5",
+    fontWeight: "bold",
+    display: "flex",
+    justifyContent: "center",
+    textShadow: "5px 0 black"
+  },
+  subtext: {
+    display: "flex",
+    marginBottom: "15px",
+    width: "60em",
+    margin: "auto auto",
+    textAlign: "center",
+    verticalAlign: "middle"
+  },
+  getstarted: {
+    fontWeight: "bold",
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: "10px"
+
+  },
+  body: {
+    padding: "30px 30px"
   },
   input: {
-    background: "white",
+    background: "white"
   },
   inputBar: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
-    // paddingBottom: "50px",
-    // borderBottom: "1px solid rgba(0,0,0,0.2)",
+    alignItems: "center"
+  },
+  searchButton: {
+    color: "white", 
+    background: "#3f51b5"
+  },
+  logoutButton: {
+    float: "right",
+    textTransform: "none"
+  },
+  searchResultText: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "50px",
+    opacity: "20%",
+    fontSize: "2rem",
+    fontWeight: "normal"
   },
   loadingCircle: {
     display: "flex",
     justifyContent: "center",
     marginTop: "50px"
-  },
-  logoutButton: {
-    float: "right",
-    textTransform: "none",
   },
 });
 
@@ -43,36 +89,43 @@ export default function LandingPage(props) {
   const classes = useStyles();
 
   const [searchTerm, setSearchTerm] = useState("");
-  const [data, setData] = useState([]);
-  const [flag, setFlag] = useState(false);
   const [staticSearchTerm, setStaticSearchTerm] = useState("")
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    console.log("use effect");
-  }, [data]);
+  useEffect(() => {}, [data]);
 
   const onSubmit = async (event) => {
+
     //prevent refresh
     event.preventDefault();
 
-    setFlag(true);
+    //display loading circle
+    setLoading(true);
+
+    //reset data
     setData([]);
 
     //API Post request
-    const response = await Axios.post("http://localhost:5000/search", {
-      searchTerm,
-    });
+    const response = await Axios.post("http://localhost:5000/search", { searchTerm });
 
+    //set head title
     setStaticSearchTerm(searchTerm)
-    setFlag(false);
+
+    //hide loading circle
+    setLoading(false);
+
+    //set data
     setData(response.data.data);
   };
 
   const onLogout = async (event) => {
+
     //prevent refresh
     event.preventDefault();
 
     try {
+
       //API Get request
       await Axios.get(`http://localhost:5000/logout`);
 
@@ -81,50 +134,60 @@ export default function LandingPage(props) {
 
       //redirect to login page
       props.history.push("/login");
+
     } catch (err) {
+      
+      //error handling
       console.log(err);
+
     }
   };
 
   return (
     <Container fixed className={classes.container}>
-      <div style={{background:'rgba(0,0,0,0.05)', paddingBottom: "50px", borderBottom: "3px solid rgba(0,0,0,1)"}}>
-
-      <Button
-        onClick={onLogout}
-        className={classes.logoutButton}
-        color="default"
-        startIcon={<ExitToAppIcon />}
-        >
-        Logout
-      </Button>
-
-      <form onSubmit={onSubmit}>
-        <Grid container spacing={3} className={classes.inputBar}>
-          <Grid item xs={6}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              autoFocus="true"
-              placeholder="Search for a company name"
-              className={classes.input}
-              value={searchTerm}
-              onChange={(event) => setSearchTerm(event.target.value)}
-              ></TextField>
+      <div className={classes.header}>
+        <Button
+          onClick={onLogout}
+          className={classes.logoutButton}
+          color="default"
+          startIcon={<ExitToAppIcon />}
+          >
+          Logout
+        </Button>
+        <Typography variant="h2" className={classes.title}>NCN</Typography>
+        <Typography variant="h8" className={classes.subtext}>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book
+        </Typography>
+        <Typography variant="h6" className={classes.getstarted}>Get started by typing in a company name:</Typography>
+        <form onSubmit={onSubmit}>
+          <Grid container spacing={3} className={classes.inputBar}>
+            <Grid item xs={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                size="small"
+                autoFocus="true"
+                placeholder="Search for a company name"
+                className={classes.input}
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+                ></TextField>
+            </Grid>
+            <Grid>
+              <IconButton className={classes.searchButton} type="submit">
+                <SearchIcon />
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid>
-            <IconButton type="submit">
-              <SearchIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-      </form>
-    </div>
-      <br></br>
-      <div>
-        {!flag & data.length != 0 ? <h1>Search results for '{staticSearchTerm}'</h1> : null}
-        {flag ? (
+        </form>
+      </div>
+      <div className={classes.body}>
+        {!loading & data.length != 0 ? <h1>Search results for '{staticSearchTerm}'</h1> : null}
+        {!loading & !data.length ? <Typography className={classes.searchResultText}>Search results will appear here</Typography> : null}
+        {loading ? (
           <div className={classes.loadingCircle}>
             <CircularProgress size="5rem"/>
           </div>
